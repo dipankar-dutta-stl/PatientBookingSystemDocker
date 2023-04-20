2f800d35f69266511053cbfce13eaac4007a729d
dipankar-dutta-stl <dipankar.dutta@stl.tech>
2023-04-20 12:32:45 +0530

    200420231232

---
+++ dist/patient-appointment-booking-system/assets/JsScripts/customJs.js
@@ -0,0 +1,10 @@
+
+function navToggle() {
+  if (document.getElementById("navbarSupportedContent").display == "block") {
+    document.getElementById("navbarSupportedContent").display = "none";
+    alert("if");
+  }
+
+}
+
+
